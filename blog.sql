5843529fdc908b23e16c9942f6383ca2890a83e8
Salted fish have dreams <269401809@qq.com>
2023-09-25 17:51:03 +0800

    feat: project configuration development in progress

---
+++ blog.sql
@@ -0,0 +1,180 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : salted_fish
+ Source Server Type    : MySQL
+ Source Server Version : 80022
+ Source Host           : localhost:3306
+ Source Schema         : blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 80022
+ File Encoding         : 65001
+
+ Date: 22/09/2023 16:08:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `dict`;
+CREATE TABLE `dict` (
+  `id` int NOT NULL AUTO_INCREMENT,
+	`dict_code` VARCHAR(64) NOT NULL COMMENT "字典code",
+	`dict_name` varchar(64) NOT NULL COMMENT '字典名称',
+	`dict_value` varchar(128) NOT NULL COMMENT "字典值",
+	`parent_id` int COMMENT "父级字典id",
+	 PRIMARY KEY (`id`) USING BTREE,
+	 UNIQUE INDEX `dict_code`(`dict_code`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+DROP TABLE IF EXISTS `category_user`;
+CREATE TABLE `category_user` (
+	`user_id` BIGINT NOT NULL,
+	`category_id` INT NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`category_name` varchar(64) UNIQUE NOT NULL COMMENT '分类名称',
+	`category_icon`varchar(30) NULL DEFAULT NULL COMMENT "分类图标",
+	`category_icon_url` varchar(128) NULL DEFAULT NULL COMMENT "分类网络图标",
+	`parent_id` int DEFAULT NULL COMMENT "父级分类id",
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for community
+-- ----------------------------
+DROP TABLE IF EXISTS `community`;
+CREATE TABLE `community`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `community_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '社区名称',
+  `community_description` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '社区描述',
+	`community_icon`varchar(30) NULL DEFAULT NULL COMMENT "社区图标",
+	`community_icon_url` varchar(128) NULL DEFAULT NULL COMMENT "社区网络图标",
+  `sort` int DEFAULT NULL COMMENT "查询排序,值越大，越靠前",
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `community_name`(`community_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission`  (
+  `id` smallint NOT NULL AUTO_INCREMENT,
+  `permission_code` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限code',
+  `permission_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限名',
+  `permission_icon` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限图标',
+  `permission_icon_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限网络图标',
+  `parent_id` smallint NULL DEFAULT NULL COMMENT '父级权限id',
+  `parent_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父级权限名',
+  `permission_remark` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限备注',
+  `permission_type` tinyint NOT NULL COMMENT '权限类型 0->菜单 1->按钮',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_code`(`permission_code`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for post
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `post_title` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '帖子标题',
+  `post_description` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '帖子描述',
+  `post_content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '帖子内容',
+  `author_id` bigint NOT NULL COMMENT '帖子作者 user_id',
+  `community_id` int NOT NULL COMMENT '帖子所属社区',
+	`category_id` int NOT NULL COMMENT '帖子在作者个人分类的位置',
+  `post_status` tinyint NULL DEFAULT 1 COMMENT '帖子状态',
+  `post_views` bigint NULL DEFAULT 0 COMMENT '帖子查看次数',
+  `post_likes` bigint NULL DEFAULT 0 COMMENT '帖子点赞次数',
+	`post_dislikes`bigint NULL DEFAULT 0 COMMENT '帖子点踩次数',
+  `sort` int DEFAULT NULL COMMENT "查询排序,值越大，越靠前",
+	`create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_author_id`(`author_id`) USING BTREE,
+  INDEX `idx_community_id`(`community_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for post_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `post_tag`;
+CREATE TABLE `post_tag`  (
+  `tag_id` int NOT NULL COMMENT '标签id',
+  `post_id` bigint NOT NULL COMMENT '帖子id'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL DEFAULT 0,
+  `role_nickname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `role_description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_role_id`(`role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission`  (
+  `permission_id` smallint NOT NULL COMMENT '权限表\"id\"字段',
+  `role_id` int NOT NULL COMMENT '角色表\"role_id\"字段'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for tag
+-- ----------------------------
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标签名',
+	`sort` int DEFAULT NULL COMMENT "查询排序,值越大，越靠前",
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `tag_name`(`tag_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+  `avatar` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '头像路径',
+  `gender` tinyint DEFAULT 0,
+	`personal_signature` varchar(64) DEFAULT "" COMMENT "个性签名",
+	`homepage_content` text COMMENT "个人主页内容展示",
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `nick_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户昵称',
+  `role_id` int NOT NULL DEFAULT 0 COMMENT '角色id',
+	`sort` int DEFAULT NULL COMMENT "查询排序,值越大，越靠前",
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_username`(`username`) USING BTREE,
+  UNIQUE INDEX `idx_user_id`(`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"blog/config"
+	"blog/dao/mysql"
+	"blog/dao/redis"
+	"blog/pkg/logger"
+	"blog/pkg/snowflake"
+	"blog/router"
+)
+
+func main() {
+	if err := config.Init(os.Args...); err != nil {
+		fmt.Printf("config init err: %v", err)
+		return
+	}
+
+	if err := mysql.Init(*config.Config.Mysql); err != nil {
+		logger.Error("mysql init err: %v", err)
+		return
+	}
+	defer mysql.Close()
+
+	if err := redis.Init(*config.Config.Redis); err != nil {
+		logger.Error("redis init err: %v", err)
+		return
+	}
+	defer redis.Close()
+
+	if err := snowflake.Init(config.Config.StartTime, config.Config.MachineId); err != nil {
+		logger.Error("snowflake init err: %v", err)
+		return
+	}
+
+	r := router.Init(config.Config.Mode)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%v", config.Config.Port),
+		Handler: r,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("server start err: %v", err)
+		}
+	}()
+
+	// 服务关闭接受通道
+	quit := make(chan os.Signal, 1)
+
+	// kill 默认会发送 syscall.SIGTERM 信号
+	// kill -2 发送 syscall.SIGINT 信号，我们常用的Ctrl+C就是触发系统SIGINT信号
+	// kill -9 发送 syscall.SIGKILL 信号，但是不能被捕获，所以不需要添加它
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit // 等待关闭信号
+
+	logger.Info("shutdown server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// 五秒超时context
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Error("server shutdown err: %v", err)
+	}
+
+	logger.Info("Bye~")
+}
